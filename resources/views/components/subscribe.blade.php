71f3ec1b9b3adb8be85986215260c84b6781cdec
TopDev <stoyanovchristian@gmail.com>
2025-03-21 18:41:13 +0800

    Update All Button Color of Frontend

--- resources/views/components/subscribe.blade.php
+++ resources/views/components/subscribe.blade.php
@@ -4,7 +4,7 @@
             <h2 class="text-center">Subscribe with us</h2>
             <form class="d-flex">
                 <input class="form-control border-0" placeholder="Enter your email Address">
-                <button class="text-white" type="submit">subscribe</button>
+                <button class="subscribe-btn text-white" type="submit">subscribe</button>
             </form>
         </div>
     </div>
